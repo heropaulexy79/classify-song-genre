6059e41d728bdf1a80dde4a76478d3d3f92187b9
heropaulexy79 <okeoluwaseun79@gmail.com>
2023-07-17 11:33:10 +0100

    Made few changes

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ Created on Fri Jul 14 17:02:27 2023
 import streamlit as st
 import subprocess
 
-def run_jupyter_notebook(notebook_path):
-    cmd = f"jupyter nbconvert --execute --to notebook --inplace {notebook_path}"
-    subprocess.call(cmd, shell=True)
-    
 notebook_path = r'C:\Users\USER\Desktop\Python apps\workspace\workspace'
+
+def run_jupyter_notebook(notebook_path):
+    #cmd = f"jupyter nbconvert --execute --to notebook --inplace {notebook_path}"
+    cmd = fr"C:\Users\USER\AppData\Local\anaconda3\Scripts\jupyter.exe nbconvert --execute --to notebook --inplace {notebook_path}"
+
+    subprocess.call(cmd, shell=True)
 
 run_jupyter_notebook(notebook_path)
 
